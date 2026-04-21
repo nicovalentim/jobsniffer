4ceacc9d65764673a20d0bc22c968629d8f74c5f
Nico <nico@outlook.com>
2026-04-21 19:14:50 -0300

    código reorganizado

--- js/vagasTemplate.js
+++ js/vagasTemplate.js
@@ -1,19 +1,52 @@
-function renderRequisitos(requisitosString) {
-    return requisitosString
-        .split(', ')
-        .map(req => `<p class="requisitos">#${req}</p>`)
-        .join('');
+export function gerarHtmlVagas(listaVagas, limiteDesc) {
+    let conteudoHTML = "";
+
+    for (const vaga of listaVagas) {
+        let descricaoCurta = vaga.descricao;
+        if (descricaoCurta.length > limiteDesc) {
+            descricaoCurta = `${descricaoCurta.slice(0, limiteDesc)} (...)`;
+        }
+
+        const requisitos = vaga.requisitos.split(', ')
+        let reqs = ""
+        for (let i = 0; i < requisitos.length; i++) {
+            reqs += `<p class="requisitos">#${requisitos[i]}</p>`
+        }
+
+        const infoVaga = criarInfoVaga(vaga, reqs);
+        const cardVaga = criarVagaCard(vaga, descricaoCurta, reqs);
+
+        conteudoHTML += cardVaga + infoVaga;
+    }
+    
+    return conteudoHTML;
 }
 
-export function templateVagaCompleta(vaga) {
-    const tamanhoDesc = 169;
-    const descricaoCurta = vaga.descricao.length > tamanhoDesc 
-        ? vaga.descricao.substring(0, tamanhoDesc) + "(...)" 
-        : vaga.descricao;
-    
-    const reqs = renderRequisitos(vaga.requisitos);
+export function criarVagaCard(vaga, descricao, reqs) {
+    return `
+            <section
+                href="#"
+                class="vaga"
+                id="vaga_${vaga.id}"
+                data-localizacao="${vaga.localizacao.toLowerCase()}"
+                data-regime="${vaga.regime.toLowerCase()}"
+                data-area="${vaga.area.toLowerCase()}">
+                    <span class="tituloVaga">
+                        <p class="areaVaga" data-area="${vaga.area.toLowerCase()}">${vaga.area}</p>
+                        <h1>${vaga.titulo}</h1>
+                    </span>
 
-    const infoVaga = `
+                    <span class="textoVaga">${descricao}</span>
+                    <hr />
+                    <span class="reqs">${reqs}</span>
+                    <span class="naVaga">${vaga.localizacao}</span>
+                    <span class="naVaga">${vaga.regime}</span>
+            </section>
+            `
+}
+
+export function criarInfoVaga(vaga, reqs) {
+    return `
         <div class="infoVaga" id="infoVaga_${vaga.id}">
             <span class="tituloVaga">
                 <p class="areaVaga" data-area="${vaga.area.toLowerCase()}">${vaga.area}</p>
@@ -25,45 +58,7 @@ export function templateVagaCompleta(vaga) {
                 <span class="naVaga">${vaga.regime}</span>
             </section>
             <span class="reqs">${reqs}</span>
-            <button>Candidatar-se!</button>
+            <button class="">Candidatar-se!</button>
         </div>
-        `;
-
-    const cardVaga = `
-        <section class="vaga" id="vaga_${vaga.id}"
-            data-localizacao="${vaga.localizacao.toLowerCase()}"
-            data-regime="${vaga.regime.toLowerCase()}"
-            data-area="${vaga.area.toLowerCase()}">
-            <span class="tituloVaga">
-                <p class="areaVaga" data-area="${vaga.area.toLowerCase()}">${vaga.area}</p>
-                <h1>${vaga.titulo}</h1>
-            </span>
-            <span class="textoVaga">${descricaoCurta}</span>
-            <hr />
-            <span class="reqs">${reqs}</span>
-            <span class="naVaga">${vaga.localizacao}</span>
-            <span class="naVaga">${vaga.regime}</span>
-        </section>
-        `;
-
-    return cardVaga + infoVaga;
-}
-
-export function templateVagaHome(vaga) {
-    const reqs = renderRequisitos(vaga.requisitos);
-    return `
-        <section class="vaga" id="vaga_${vaga.id}"
-            data-localizacao="${vaga.localizacao.toLowerCase()}"
-            data-regime="${vaga.regime.toLowerCase()}"
-            data-area="${vaga.area.toLowerCase()}">
-            <span class="tituloVaga">
-                <p class="areaVaga" data-area="${vaga.area.toLowerCase()}">${vaga.area}</p>
-                <h1>${vaga.titulo}</h1>
-            </span>
-            <hr />
-            <span class="reqs">${reqs}</span>
-            <span class="naVaga">${vaga.localizacao}</span>
-            <span class="naVaga">${vaga.regime}</span>
-        </section>
-        `;
+    `
 }
--- static/rsc/cidades.js
+++
@@ -1,7 +0,0 @@
-
-const estado = document.getElementsByName("estado")
-const cidadeTexto = document.getElementsByName("cidade")
-
-if (estado.value == "") {
-    cidadeTexto.value.readOnly = true;
-} // arrumar isso aqui depois
